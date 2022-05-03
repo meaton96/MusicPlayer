f34ba53028176762fd40b6b5343116a50a1564d7
Lammara <lammara96@gmail.com>
2022-05-03 18:28:59 -0400

    todo: add info button, add scrollbar for song frame if song list is too long

--- main.py
+++ main.py
@@ -5,29 +5,28 @@ from PIL.Image import Resampling
 from pygame import mixer
 import tkinter as tk
 import tkinter.filedialog
+from tkinter import ttk
 import os
 from PIL import Image, ImageTk
-
-
-# change the volume based on the slider value
-def change_volume(vol):
-    mixer.music.set_volume(int(vol) / 100)
 
 
 class MusicGUI:
 
     def __init__(self):
-        # initialize mixer and create main Tkinter window
+        # init buttons variables
+        self.mute_button = None
+        self.button_frame = None
+        self.next_button = None
+        self.previous_button = None
+        self.play_button = None
         self.quit_button = None
         self.open_folder_button = None
         self.pause_button = None
-        self.pause_text = None
         self.volume_slider = None
-        self.shuffle_button = None
-        self.back_button = None
-        self.next_button = None
-        self.repeat_button = None
+        self.cur_vol = 100
+        self.previous_vol = 0
 
+        # initialize mixer and create main Tkinter window
         mixer.init()
         self.main_window = tk.Tk(className="Music Player")
         self.main_window.geometry("800x600")
@@ -46,12 +45,14 @@ class MusicGUI:
         self.PLAY = "Play"
 
         # create basic frames
-        self.button_frame = tk.Frame(self.main_window, bg="#4596ed", width=self.WIDTH, height=150, padx=200, pady=20)
         self.song_list_frame = tk.Frame(self.main_window, width=80, height=self.main_window.winfo_height(),
                                         name="song_frame", bd=-2)
 
-        self.button_canvas = tk.Canvas(self.main_window, width=self.WIDTH,
-                                       height=60, bg="#4596ed", bd=-2)
+        self.button_frame = tk.Frame(self.main_window, bg="#33964e", width=100, height=60)
+        self.button_canvas = tk.Canvas(self.button_frame, width=self.WIDTH - 100,
+                                       height=60, bg="#33964e", bd=-2)
+
+        self.button_canvas.grid(row=0, column=0)
 
         self.top_canvas = tk.Canvas(self.song_list_frame, width=self.WIDTH, height=30, bd=-2)
 
@@ -68,57 +69,101 @@ class MusicGUI:
         # self.open_folder_button
 
         # pack frames
-        self.button_canvas.pack(side="bottom")
+        # self.button_canvas.pack()
         self.top_canvas.pack()
         self.empty_label.pack()
         self.song_list_frame.pack()
-        # self.button_frame.pack(side="bottom")
+        self.button_frame.pack(side="bottom")
 
         tk.mainloop()
 
     def create_buttons(self):
-        # pause button
-        # self.pause_text = tk.StringVar()
-        # self.pause_text.set(self.PLAY)
-        # self.pause_button = tk.Button(self.button_frame, textvariable=self.pause_text, command=self.pause_song,
-        #                              width=15)
         center_x = self.WIDTH // 2
 
-        path = os.path.abspath(os.path.dirname(__file__))
-        icon_path = os.path.join(path, "icons\\open_folder_icon.png")
+        # create a style to remove the background of the volume slider
+        style = ttk.Style()
+        style.configure('TScale', background="#33964e")
 
+        # volume slider
+        self.volume_slider = ttk.Scale(self.button_frame, from_=0, to=100, orient=tkinter.HORIZONTAL,
+                                       command=self.change_volume, style="TScale")
+        self.volume_slider.set(100)
+        self.volume_slider.grid(row=0, column=1)
+
+        path = os.path.abspath(os.path.dirname(__file__))
+
+        # create TransparentButton instances with the correct icon
+        icon_path = os.path.join(path, "icons\\open_folder_icon.png")
         self.open_folder_button = TransparentButton(self.button_canvas, 5, 15, 50, 50, image_path=icon_path,
                                                     command=self.open_dir, state=tk.NORMAL)
 
         icon_path = os.path.join(path, "icons\\quit_icon.png")
-
         self.quit_button = TransparentButton(self.top_canvas, 2, 2, 30, 30, image_path=icon_path,
-                                             command=self.main_window.destroy)
+                                             command=self.main_window.destroy, state=tk.NORMAL)
 
         icon_path = os.path.join(path, "icons\\play_button_icon.png")
-
         self.play_button = TransparentButton(self.button_canvas, center_x - 25, 5, 50, 50,
-                                             image_path=icon_path, command=self.pause_song)
+                                             image_path=icon_path, command=self.pause_song, state=tk.NORMAL)
 
         icon_path = os.path.join(path, "icons\\pause_button_icon.png")
-
         self.pause_button = TransparentButton(self.button_canvas, center_x - 25, 5, 50, 50,
                                               image_path=icon_path, command=self.pause_song, state=tk.HIDDEN)
 
-        # volume slider
-        self.volume_slider = tk.Scale(self.button_frame, from_=0, to=100, orient=tkinter.HORIZONTAL, width=10,
-                                      command=change_volume)
-        self.volume_slider.set(100)
-        self.volume_slider.pack()
+        icon_path = os.path.join(path, "icons\\previous_button_icon.png")
+        self.previous_button = TransparentButton(self.button_canvas, center_x - 60, 19, 30, 30, image_path=icon_path,
+                                                 command=self.prev_song, state=tk.NORMAL)
+
+        icon_path = os.path.join(path, "icons\\next_button_icon.png")
+        self.next_button = TransparentButton(self.button_canvas, center_x + 35,  19, 30, 30, image_path=icon_path,
+                                             command=self.next_song, state=tk.NORMAL)
+        
+        icon_path = os.path.join(path, "icons\\volume_icon.png")
+        self.mute_button = TransparentButton(self.button_canvas, 660, 19, 30, 30,
+                                             image_path=icon_path, command=self.mute, state=tk.NORMAL)
+
+    # change the volume based on the slider value
+    def change_volume(self, vol):
+        mixer.music.set_volume(float(vol) / 100)
+        self.cur_vol = float(vol) / 100
+
+    # mute or unmute the volume
+    def mute(self):
+        if self.cur_vol != 0:
+            self.previous_vol = self.cur_vol
+            self.volume_slider.set(0)
+        else:
+            self.cur_vol = self.previous_vol
+            self.volume_slider.set(self.cur_vol * 100)
+
+    # move the current playing song in the specified direction
+    def skip_song(self, direction):
+        # change song list index
+        current_song = self.song_list.index(self.song_choice)
+        current_song += direction
+
+        # make sure its in bounds
+        if current_song >= len(self.song_list):
+            current_song = 0
+        elif current_song < 0:
+            current_song = len(self.song_list) - 1
+
+        # set the current song choice to the new song
+        # generate an event as if that song was clicked on to easily move song hi-light indicator
+        # call pause_song() to being playing the new song
+        self.song_choice = self.song_list[current_song]
+        self.song_labels[current_song].event_generate("<Button-1>")
+        self.is_playing_song = False
+        self.pause_song()
+
+    # play the next song in the queue
+    def next_song(self):
+        self.skip_song(1)
+    # play the previous song in the queue
+    def prev_song(self):
+        self.skip_song(-1)
 
     # pause or play a song
     def pause_song(self):
-        # path = os.path.abspath(os.path.dirname(__file__))
-        # icon_path = os.path.join(path, "icons\\open_folder_icon.png")
-        # if a song is currently playing then pause it
-        """
-        issue here with playing a new song also
-        """
         if not self.is_empty_dir:
             if self.is_playing_song:
                 mixer.music.pause()
@@ -126,7 +171,7 @@ class MusicGUI:
                 self.play_button.set_state(tk.NORMAL)
                 self.is_playing_song = False
             else:
-                # if a song isnt playing and a new song isnt being chosen to play then unpause the song
+                # if a song isn't playing and a new song isn't being chosen to play then unpause the song
                 if self.current_song == self.song_choice:
                     mixer.music.unpause()
                 # if a new song has been selected to play then load that song and play it
@@ -138,7 +183,6 @@ class MusicGUI:
                 self.pause_button.set_state(tk.NORMAL)
                 self.play_button.set_state(tk.HIDDEN)
                 self.is_playing_song = True
-
 
     # open a directory to play music from
     def open_dir(self):
@@ -154,8 +198,11 @@ class MusicGUI:
             # print an error message if the opened folder was empty
         except FileNotFoundError:
             self.empty_label_text.set("Directory not found please try again")
+
         # create labels and add each file to the song list
         count = 0
+        self.song_list = []
+        self.song_labels = []
         for fileName in files:
             self.song_list.append(fileName)
             self.song_labels.append(
@@ -194,18 +241,13 @@ class MusicGUI:
                 int(str(widget).strip(".song_frame."))]
 
 
-# Had a lot of issues trying to create transparent background buttons
-# came across this solution from
-# https://stackoverflow.com/questions/70996098/tkinter-button-over-transparent-background/71011331#71011331
-# changed a bit to fit my needs
+#
+#
+#
 class TransparentButton:
-    """ Create leftmost mouse button clickable canvas image object.
+    # flash_delay = 100  # Milliseconds.
 
-    The x, y coordinates are relative to the top-left corner of the canvas.
-    """
-    flash_delay = 100  # Milliseconds.
-
-    def __init__(self, canvas, x, y, width, height, image_path, command, state=tk.NORMAL):
+    def __init__(self, canvas, x, y, width, height, image_path, command, state):
         self.x = x
         self.y = y
         self.width = width
@@ -217,7 +259,7 @@ class TransparentButton:
         self.btn_image = ImageTk.PhotoImage(resized_image)
         self.canvas_btn_img_obj = canvas.create_image(x, y, anchor='nw', state=state,
                                                       image=self.btn_image)
-        canvas.tag_bind(self.canvas_btn_img_obj, "<ButtonRelease-1>", lambda event: (self.flash(), command()))
+        canvas.tag_bind(self.canvas_btn_img_obj, "<ButtonRelease-1>", lambda e: command())
 
     def change_icon(self, image_path):
         self.image = Image.open(image_path)
@@ -225,17 +267,11 @@ class TransparentButton:
         self.btn_image = ImageTk.PhotoImage(resized_image)
         self.canvas_btn_img_obj.config(image=self.btn_image)
 
-    def flash(self):
-        self.set_state(tk.HIDDEN)
-        self.canvas.after(self.flash_delay, self.set_state, tk.NORMAL)
+    # def flash(self):
+    #    self.set_state(tk.HIDDEN)
+    #   self.canvas.after(self.flash_delay, self.set_state, tk.NORMAL)
 
     def set_state(self, state):
-        """ Change canvas button image's state.
-
-        Normally, image objects are created in state tk.NORMAL. Use value
-        tk.DISABLED to make it unresponsive to the mouse, or use tk.HIDDEN to
-        make it invisible.
-        """
         self.canvas.itemconfigure(self.canvas_btn_img_obj, state=state)
 
 
